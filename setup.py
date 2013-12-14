5ef0fa16d3ad56312b26316ca1764d7bfd93a85a
Georg Walther <georgwalther@gmail.com>
2013-12-14 09:43:47 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+kargs = {}
+
+kargs['name'] = 'PLoSPy'
+kargs['version'] = ''
+kargs['author'] = 'Georg R Walther'
+kargs['author_email'] = 'contact@georg.io'
+kargs['packages'] = 'plospy'
+kargs['package_dir'] = {'plospy': 'plospy'}
+kargs['url'] = 'http://georg.io'
+kargs['license'] = 'BSD'
+kargs['description'] = 'A library for handling PLoS data'
+kargs['long_description'] = ''
+kargs['test_suite'] = 'plospy.tests'
+kargs['install_requires'] = ['BeautifulSoup']
+
+setup(**kargs)
